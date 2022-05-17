d75a990a3033a4380a8be450275a531b754e5bc9
Simon Wörpel <simon.woerpel@medienrevolte.de>
2022-05-17 03:37:02 +0200

    Disable client cache via headers

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/recipient/:id",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
