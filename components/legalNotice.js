0fef493364ff81327162d8c1b44d5fed6ab3632c
Simon Wörpel <simon.woerpel@pm.me>
2025-06-03 12:10:06 +0200

    📝 Add data agreement notice & document

--- components/legalNotice.js
+++ components/legalNotice.js
@@ -19,9 +19,25 @@ export default function LegalNotice({ variant = "secondary" }) {
         two years ({years}).
       </p>
       <p>
-        Please <Link href={loginUrl}>login</Link> if you have an account our{" "}
-        <Link href="mailto:farmsubsidy@okfn.de">contact us</Link> if you are a
-        researcher or journalist to get access to the full data.
+        Please <Link href={loginUrl}>login</Link> if you have an account our request one.
+      </p>
+      <p>
+        To get access to the complete data as researcher or journalist, please
+        print out the <a
+        href="/farmsubsidy_datausage_agreement_2023_EN.pdf">data usage
+        agreement</a>, sign it and send the paper version via post to:
+      </p>
+      <p>
+        Open Knowledge Foundation Deutschland e.V.<br />
+        Singerstraße 109<br />
+        D-10179 Berlin
+      </p>
+      <p>
+        As soon as we received the original document we&apos;ll send you the
+        login by email. You can also scan the signed document and send it to{" "}
+        <a href="mailto:farmsubsidy@okfn.de">farmsubsidy@okfn.de</a> beforehand
+        so that you receive your login data earlier. Nevertheless, you have to
+        send the original to the adress above, too.
       </p>
     </Alert>
   );
